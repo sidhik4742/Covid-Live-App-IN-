06c62df964e054358c0f978b7c0c1b59204cc06c
sidhik4742 <sidhik64742@gmail.com>
2020-06-04 22:53:55 +0530

    Added card with all state report

--- js/index.js
+++ js/index.js
@@ -9,6 +9,9 @@ const url_covidNotification = 'https://api.rootnet.in/covid19-in/notifications'
 
 let notification = [];
 var randomNum = [];
+var sateName =[];
+// var stateName_number;
+// let state_totalCase;
 for(var counter =1;counter<=20;counter++){
     randomNum.push(Math.floor((Math.random() * 20) + 1));
 }
@@ -60,22 +63,32 @@ async function getCovidData(){
         
     // });
     //************************Content added drop-down list*******************//
-    for(var state_name_counter =0;state_name_counter<state_name_totalCases.length;state_name_counter++){
+    
+    for(var state_name_counter =0;state_name_counter<data_covidStatus.data.regional.length;state_name_counter++){
+        sateName.push(state_name_totalCases[state_name_counter].state_name);
         let state_name_id = document.getElementById("stateWise");
         let option = document.createElement("option");
         option.text = state_name_totalCases[state_name_counter].state_name;
         option.classList.add("dropdown-item");
+        option.value = state_name_totalCases[state_name_counter].state_name;
+        option.addEventListener('click',function(){
+            // console.log(option.value);
+            let stateName_number = (sateName.indexOf(option.value));
+            let state_totalCase = data_covidStatus.data.regional[stateName_number].totalConfirmed;
+            let state_discharged = data_covidStatus.data.regional[stateName_number].discharged;
+            let state_deaths = data_covidStatus.data.regional[stateName_number].deaths;
+            let state_activeCases = (state_totalCase-(state_discharged+state_deaths));
+            // console.log(state_activeCases);
+            
+            document.getElementById("totalCases").innerHTML = state_totalCase;
+            document.getElementById("activeCases").innerHTML = state_activeCases;
+            document.getElementById("discharged").innerHTML = state_discharged;
+            document.getElementById("deaths").innerHTML = state_deaths;
+        });
         state_name_id.appendChild(option);
+        // console.log(option);
     }
     
-    // console.log(state_name_id);
-    
-    
-    
-    
-    // console.log(option);
-    
-
     //********************Random selected 20 Covid Notifications******************//
     // console.log(data_covidNotification.data.notifications[20]);
 
@@ -87,26 +100,26 @@ async function getCovidData(){
     //     console.log(item);
     // });
 
-    document.getElementById("updation").innerHTML = "Last update:" + lastOriginUpdate + "(GMT+5:30)";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-    document.getElementById("totalCases").innerHTML = totalCases;
-    document.getElementById("activeCases").innerHTML = activeCases;
-    document.getElementById("discharged").innerHTML = discharged;
-    document.getElementById("deaths").innerHTML = deaths;
-
-    state_name_totalCases.forEach(function(item,index,array) {
-        document.getElementById("state_name_totalCases").innerHTML += item.state_name+":"+item.state_cases+"\t|\t"; 
-    });
+    document.getElementById("updation").innerHTML = "Last update:" + lastOriginUpdate + "(GMT+5:30)";
 
     document.getElementById("notificationNum").innerHTML = notification.length;
     notification.forEach(function(item,index,array) {
         document.getElementById("notifications").innerHTML += item.title+"\n"+item.link; 
     });
 
+    
+    state_name_totalCases.forEach(function(item,index,array) {
+        document.getElementById("state_name_totalCases").innerHTML += item.state_name+":"+item.state_cases+"\t|\t"; 
+    });
+    
+    document.getElementById("totalCases").innerHTML = totalCases;
+    document.getElementById("activeCases").innerHTML = activeCases;
+    document.getElementById("discharged").innerHTML = discharged;
+    document.getElementById("deaths").innerHTML = deaths;
 
     
-
-    
-}
+    }
+    // console.log(sateName);
 });
     
    
